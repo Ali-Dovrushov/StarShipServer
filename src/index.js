c268fc0d295975adf9c8abaa939ab1618050602f
MusaW <musa@wizardsdev.com>
2020-12-29 10:53:22 +0200

    add ws connection

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function init() {
     const port = 3002 || process.env.PORT;
     const db = new Model();
     const app = new App(db);
-    const server = new Server(port, app);
+    const server = new Server(port, app, db);
 
     server.start();
 }
